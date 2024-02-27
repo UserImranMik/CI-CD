bf164006385a53adbf1a3fbd49e8fc1dbd718040
UserImranMik <imrankhan.m@infocareerindia.com>
2024-02-27 13:05:07 +0530

    Success

--- src/main/java/com/boot/controller/ApiController.java
+++ src/main/java/com/boot/controller/ApiController.java
@@ -1,5 +1,8 @@
 package com.boot.controller;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @CrossOrigin("*")
 public class ApiController {
+	
 	@GetMapping("/get")
 	public String demo() throws InterruptedException {
-		Thread.sleep(3000);
+		
 		return "result from get api";
 	}
+	
+	@GetMapping("/green")
+	public String demoGreen() throws InterruptedException {
+	
+		LocalDateTime timestamp = LocalDateTime.now();
+		
+		return "result from get GREEN api" + timestamp;
+	}
+	
 	@PostMapping("/post")
 	public String post() {
 		return "result from post api";
 	}
+	
 	@PutMapping("/put")
 	public String put() {
 		return "result from put api";
